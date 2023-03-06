7228e6c46c579d0eb8e0b45480a6720ad46c92a9
roiaharon-pepperi <roi.a@pepperi.com>
2023-03-06 17:45:20 +0200

    DI-22066 dynamic getter
    remove FilterAsync function, add optional getter function to filter

--- filters/filter-collection.ts
+++ filters/filter-collection.ts
@@ -29,20 +29,6 @@ export default class FilterCollection extends Filter {
         return res;
     }
 
-    async filterAsync(object: any) {
-        let res = true;
-
-        for (const filter of this.filters) {
-            res = await filter.filterAsync(object);
-
-            if (res != this.useAndOperation) {
-                break;
-            }
-        }
-
-        return res;
-    }
-
     toSQLWhereClause(): string {
         let res = '';
 
