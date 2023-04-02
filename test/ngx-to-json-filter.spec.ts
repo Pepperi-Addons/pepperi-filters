59d423a0a4a5edb77dbe6b91777c584d2444dcf4
michael.a@pepperi.com <michael.a@pepperi.com>
2023-04-02 13:57:51 +0300

    add string tests

--- test/ngx-to-json-filter.spec.ts
+++ test/ngx-to-json-filter.spec.ts
@@ -2,7 +2,7 @@ import 'mocha';
 import { expect } from 'chai';
 import { IPepSmartFilterData } from '../ngx-filters/json-to-ngx/ngx-types';
 import { SchemeFieldType } from '../ngx-filters/ngx-to-json/metadata';
-import { JSONDoubleFilter, JSONIntegerFilter, JSONRegularFilter } from '../json-filter';
+import { JSONDoubleFilter, JSONIntegerFilter, JSONRegularFilter, JSONStringFilter } from '../json-filter';
 import { ngxFilterToJsonFilter } from '../index';
 describe('NGX Filters To JSON Filters', () => {
     describe('1. Integer Filters', () => {
@@ -283,6 +283,178 @@ describe('NGX Filters To JSON Filters', () => {
             it(`${index + 1}. ${ngxFilter.fieldId} test `, getNGXFilterTestCB(ngxFilter, expectedResults[index], type))
         })
     });
+    describe('3. String Filters', () => {
+        const type: SchemeFieldType = 'String'
+        const ngxIntegerFilters: IPepSmartFilterData[] = [
+            {
+                fieldId: 'equal to A',
+                operator: {
+                    id: 'eq',
+                    name: 'EQUAL',
+                    short: "=",
+                    componentType: ["number", "boolean", "text"]
+                },
+                value: {first: 'A'} 
+            },
+            {
+                fieldId: 'equal to B',
+                operator: {
+                    id: 'eq',
+                    name: 'EQUAL',
+                    short: "=",
+                    componentType: ["number", "boolean", "text"]
+                },
+                value: {first: 'B'} 
+            },
+            {
+                fieldId: 'not equal to A',
+                operator: {
+                    id: 'neq',
+                    name: 'NOT_EQUAL',
+                    short: "<>",
+                    componentType: ["number", "text"]
+                },
+                value: {first: 'A'} 
+            },
+            {
+                fieldId: 'not equal to B',
+                operator: {
+                    id: 'neq',
+                    name: 'NOT_EQUAL',
+                    short: "<>",
+                    componentType: ["number", "text"]
+                },
+                value: {first: 'B'} 
+            },
+            {
+                fieldId: 'contains A',
+                operator: {
+                    id: 'contains',
+                    name: 'CONTAINS',
+                    short: "Contains",
+                    componentType: ["text"]
+                },
+                value: {first: 'A'} 
+            },
+            {
+                fieldId: 'contains B',
+                operator: {
+                    id: 'contains',
+                    name: 'CONTAINS',
+                    short: "Contains",
+                    componentType: ["text"]
+                },
+                value: {first: 'B'} 
+            },
+            {
+                fieldId: 'begins with A',
+                operator: {
+                    id: 'beginsWith',
+                    name: 'BEGINS_WITH',
+                    short: "Begins With",
+                    componentType: ["text"]
+                },
+                value: {first: 'A'} 
+            },
+            {
+                fieldId: 'begins with B',
+                operator: {
+                    id: 'beginsWith',
+                    name: 'BEGINS_WITH',
+                    short: "Begins With",
+                    componentType: ["text"]
+                },
+                value: {first: 'B'} 
+            },
+            {
+                fieldId: 'ends with A',
+                operator: {
+                    id: 'endsWith',
+                    name: 'ENDS_WITH',
+                    short: "Ends With",
+                    componentType: ["text"]
+                },
+                value: {first: 'A'} 
+            },
+            {
+                fieldId: 'ends with B',
+                operator: {
+                    id: 'endsWith',
+                    name: 'ENDS_WITH',
+                    short: "Ends With",
+                    componentType: ["text"]
+                },
+                value: {first: 'B'} 
+            },
+        ]
+    
+        const expectedResults: JSONStringFilter[] = [
+            {
+                FieldType: 'String',
+                ApiName: 'equal to A',
+                Operation: 'IsEqual',
+                Values: ['A']
+            },
+            {
+                FieldType: 'String',
+                ApiName: 'equal to B',
+                Operation: 'IsEqual',
+                Values: ['B']
+            },
+            {
+                FieldType: 'String',
+                ApiName: 'not equal to A',
+                Operation: 'IsNotEqual',
+                Values: ['A']
+            },
+            {
+                FieldType: 'String',
+                ApiName: 'not equal to B',
+                Operation: 'IsNotEqual',
+                Values: ['B']
+            },
+            {
+                FieldType: 'String',
+                ApiName: 'contains A',
+                Operation: 'Contains',
+                Values: ['A']
+            },
+            {
+                FieldType: 'String',
+                ApiName: 'contains B',
+                Operation: 'Contains',
+                Values: ['B']
+            },
+            {
+                FieldType: 'String',
+                ApiName: 'begins with A',
+                Operation: 'StartWith',
+                Values: ['A']
+            },
+            {
+                FieldType: 'String',
+                ApiName: 'begins with B',
+                Operation: 'StartWith',
+                Values: ['B']
+            },
+            {
+                FieldType: 'String',
+                ApiName: 'ends with A',
+                Operation: 'EndWith',
+                Values: ['A']
+            },
+            {
+                FieldType: 'String',
+                ApiName: 'ends with B',
+                Operation: 'EndWith',
+                Values: ['B']
+            },
+        ]
+        ngxIntegerFilters.forEach((ngxFilter, index) => {
+            it(`${index + 1}. ${ngxFilter.fieldId} test `, getNGXFilterTestCB(ngxFilter, expectedResults[index], type))
+        })
+    });
+    
 })
 
 
